c029fa9528e1a217612eaf071667a2de365e05ba
Vin Busquet <computationalcore@gmail.com>
2019-02-24 08:36:00 -0300

    feat: Add Question and QuestionOptionItem components

---
+++ src/components/Question.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+import QuestionOptionItem from './QuestionOptionItem';
+
+/**
+ * This object is used for type checking the props of the component.
+ */
+const propTypes = {
+  category: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['boolean', 'multiple']).isRequired,
+  difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']).isRequired,
+  question: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+/**
+ * This object sets default values to the optional props.
+ */
+const defaultProps = {
+  onItemSelected: () => { },
+};
+
+/**
+ * @callback onItemSelected
+ * @param {string} selected - Text of the option selected.
+ */
+
+/**
+ * @description	Represents a individual Quiz Question.
+ * @constructor
+ * @param {Object} props - The props that were defined by the caller of this component.
+ * @param {string} props.category - The id of the book.
+ * @param {string} props.type - The url of the book cover image.
+ * @param {string} props.difficulty - The title of the book.
+ * @param {boolean} props.question - Indicates whether the book is updating. Shows the loader layer if true.
+ * @param {} props.onItemSelected - Executed when an option is selected.
+*/
+function Question(props) {
+  return (
+    <View style={styles.questionDataContainer}>
+
+      <View style={styles.questionData}>
+
+        <Text style={styles.questionDescription}>{props.question}</Text>
+
+      </View>
+
+      <FlatList
+          style={styles.questionOptions}
+          data={props.options}
+          contentContainerStyle={styles.questionOptionsContainer}
+          renderItem={({ item }) => (
+            <QuestionOptionItem
+              optionDescription={item}
+              onPressItem={props.onItemSelected}
+            />
+          )}
+          keyExtractor={(item, index) => `${index}-${item}`}
+          onPressItem={props.onItemSelected}
+          scrollEnabled={false}
+        />
+    </View>
+  );
+}
+
+/**
+ * Question component StyleSheet.
+ */
+const styles = StyleSheet.create({
+  questionDataContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 16,
+    marginRight: 16,
+  },
+
+  questionData: {
+    padding: 16,
+    marginTop: 32,
+    marginBottom: 32,
+    alignSelf: 'stretch',
+    maxHeight: 280,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#ffffff',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  },
+
+  questionDescription: {
+    color: '#000',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+
+  questionOptions: {
+    width: '100%',
+  },
+  questionOptionsContainer: {
+    marginTop: 0,
+  }
+});
+
+// Type checking the props of the component
+Question.propTypes = propTypes;
+// Assign default values to the optional props
+Question.defaultProps = defaultProps;
+
+export default Question;
---
+++ src/components/QuestionOptionItem.js
@@ -0,0 +1,83 @@
+import React, { PureComponent } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+/**
+ * This object is used for type checking the props of the component.
+ */
+const propTypes = {
+  optionDescription: PropTypes.string.isRequired,
+  onPressItem: PropTypes.func.isRequired,
+};
+
+/**
+ * @callback onPressItem
+ * @param {string} selected - Text of the option selected.
+ */
+
+/**
+ * @description	Represents a individual Quiz Option.
+ * @constructor
+ * @param {Object} props - The props that were defined by the caller of this component.
+ * @param {string} props.optionDescription - The text of the option.
+ * @param {onPressItem} props.onPressItem - Executed when user selects an option.
+ */
+class QuestionOptionItem extends PureComponent {
+
+  _onPress = () => {
+    console.log('QuestionOptionItem Pressed : ');
+    console.log(this.props.optionDescription);
+    this.props.onPressItem(this.props.optionDescription);
+  };
+
+  render() {
+    const { optionDescription } = this.props;
+
+    return (
+      <TouchableOpacity
+        onPress={this._onPress}
+      >
+        <View style={styles.quizOption}>
+          <Text style={styles.quizOptionDescription}>{optionDescription}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+/**
+ * QuestionOptionItem component StyleSheet.
+ */
+const styles = StyleSheet.create({
+  quizOption: {
+    flex: 1,
+    alignSelf: 'stretch',
+    minHeight: 32,
+    marginTop: 4,
+    marginBottom: 4,
+    backgroundColor: 'rgba(64, 64, 64,0.8)',
+    borderRadius: 8,
+  },
+
+  quizOptionDescription: {
+    flex: 1,
+    padding: 12,
+    color: '#ffffff',
+    fontSize: 24,
+    fontWeight:'normal',
+    textAlign: 'center',
+    textShadowColor:'#000000',
+    textShadowOffset:{width: 2, height: 2},
+    textShadowRadius:0,
+  },
+});
+
+// Type checking the props of the component
+QuestionOptionItem.propTypes = propTypes;
+
+export default QuestionOptionItem;
